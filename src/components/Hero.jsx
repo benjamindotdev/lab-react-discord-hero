fa9aa6ee4b43d49e3e4ffbd7d9db5fcd12611110
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-09 15:15:45 +0200

    done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1 className="hero__title">IMAGINE A PLACE...</h1>
+      <p className="hero__text">
+        ...where you can belong to a school club, a gaming group, or a worldwide
+        art community. Where just you and a handful of friends can spend time
+        together. A place that makes it easy to talk every day and hang out more
+        often.
+      </p>
+      <button className="hero__button" id="hero__button-mac">
+        Download for Mac
+      </button>
+      <button className="hero__button" id="hero__button-browser">
+        Open Discord in your browser
+      </button>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img
+        src="src/assets/discord-logo-white.png"
+        alt="Discord Logo"
+        className="navbar__discord-logo"
+      />
+      <img
+        src="src/assets/menu-icon.png"
+        alt="Menu Icon"
+        className="navbar__menu-icon"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
